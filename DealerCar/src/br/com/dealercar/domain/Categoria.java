2e7e62266ad526c987cebcb63f1bcb4fbaf7aa93
legionario07 <paulinho.legionario@hotmail.com>
2015-08-26 19:50:10 -0300

    Criação do DAO Categoria

---
+++ DealerCar/src/br/com/dealercar/domain/Categoria.java
@@ -0,0 +1,62 @@
+package br.com.dealercar.domain;
+
+public class Categoria {
+
+	private int id;
+	private String nome;
+	private String descricao;
+	private Double valorDiaria;
+
+	public Categoria() {
+
+	}
+
+	public Categoria(String nome, Double valorDiaria) {
+		this.setNome(nome);
+		this.setValorDiaria(valorDiaria);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValorDiaria() {
+		return valorDiaria;
+	}
+
+	public void setValorDiaria(Double valorDiaria) {
+		this.valorDiaria = valorDiaria;
+	}
+
+	@Override
+	public String toString() {
+		String retorno;
+		retorno = "Id: " + this.getId() + "\n";
+		retorno += "Nome: " + this.getNome() + "\n";
+		retorno += "Descricao: " + this.getDescricao() + "\n";
+		retorno += "Valor Diária: " + this.getValorDiaria() + "\n\n";
+
+		return retorno;
+	}
+
+}
--- DealerCar/src/br/com/dealercar/domain/Funcionario.java
+++ DealerCar/src/br/com/dealercar/domain/Funcionario.java
@@ -4,6 +4,25 @@ public class Funcionario extends Pessoa {
 	private String cargo;
 	private Double salario;
 
+	public Funcionario(){
+		
+	}
+	
+	public Funcionario(String nome, String dataNasc, String sexo, String telefone, 
+			String celular, String cargo, Double salario, String endereco, Cidade cidade) {
+		
+		this.setNome(nome);
+		this.setDataNasc(dataNasc);
+		this.setSexo(sexo);
+		this.setTelefone(telefone);
+		this.setCelular(celular);
+		this.setEndereco(endereco);
+		this.setCidade(cidade);
+		this.setCargo(cargo);
+		this.setSalario(salario);
+		
+	}
+
 	public String getCargo() {
 		return cargo;
 	}
